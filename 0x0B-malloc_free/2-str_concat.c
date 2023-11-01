234f12bd6bc305c76bd4c55bee7f6b12bcd29b40
broxilius <hamza.elguemri@gmail.com>
2023-11-01 14:14:54 -0700

    3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,16 +27,18 @@ char *str_concat(char *s1, char *s2)
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
+
 	while (s1[i] != '\0')
 		{
 			conct[i] = s1[i];
 			i++;
-			}
+		}
+
 	while (s2[ci] != '\0')
 		{
 			conct[i] = s2[ci];
 			i++, ci++;
-			}
+		}
 	conct[i] = '\0';
 	return (conct);
 	}
